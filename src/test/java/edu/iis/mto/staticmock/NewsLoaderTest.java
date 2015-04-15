64ad2495002a4a7f80d05941b69cabfb880134fd
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2015-04-15 11:37:37 +0200

    Dodanie testu zachowania. Test sprawdza czy metoda loadConfiguration klasy ConfigurationLoader wywoływana jest tylko raz w metodzie loadNews klasy NewsLoader.

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -10,6 +10,8 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import static org.junit.Assert.*;
 import edu.iis.mto.staticmock.reader.FileNewsReader;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.reflect.Whitebox.getField;
@@ -20,15 +22,17 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @PrepareForTest({ ConfigurationLoader.class, NewsReaderFactory.class })
 public class NewsLoaderTest {
 
-	@SuppressWarnings("unchecked" )
+	@SuppressWarnings("unchecked")
 	@Test
-	public void loadNews_expectedOnlyPublicContent() throws IllegalArgumentException, IllegalAccessException {
+	public void loadNews_expectedOnlyPublicContent()
+			throws IllegalArgumentException, IllegalAccessException {
 
-		//GIVEN:
-		IncomingInfo incomingInfo = new IncomingInfo("content", SubsciptionType.NONE);
+		// GIVEN:
+		IncomingInfo incomingInfo = new IncomingInfo("content",
+				SubsciptionType.NONE);
 		IncomingNews incomingNews = new IncomingNews();
 		incomingNews.add(incomingInfo);
-		
+
 		// mock Configuration
 		Configuration configuration = mock(Configuration.class);
 		when(configuration.getReaderType()).thenReturn("File");
@@ -49,27 +53,28 @@ public class NewsLoaderTest {
 				fileNewsReader);
 
 		NewsLoader newsLoader = new NewsLoader();
-		
-		
-		//WHEN:
+
+		// WHEN:
 		PublishableNews publishableNews = newsLoader.loadNews();
 		Field field = getField(PublishableNews.class, "publicContent");
-		List<String> testPublicContent = (List<String>) field.get(publishableNews);
-		
-		
-		//THEN:
-		assertEquals(testPublicContent.size(),1);
-	}
-	
-	@SuppressWarnings("unchecked" )
-	@Test
-	public void loadNews_expectedOnlySubscribentContent() throws IllegalArgumentException, IllegalAccessException {
+		List<String> testPublicContent = (List<String>) field
+				.get(publishableNews);
 
-		//GIVEN:
-		IncomingInfo incomingInfo = new IncomingInfo("content", SubsciptionType.A);
+		// THEN:
+		assertEquals(testPublicContent.size(), 1);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void loadNews_expectedOnlySubscribentContent()
+			throws IllegalArgumentException, IllegalAccessException {
+
+		// GIVEN:
+		IncomingInfo incomingInfo = new IncomingInfo("content",
+				SubsciptionType.A);
 		IncomingNews incomingNews = new IncomingNews();
 		incomingNews.add(incomingInfo);
-		
+
 		// mock Configuration
 		Configuration configuration = mock(Configuration.class);
 		when(configuration.getReaderType()).thenReturn("File");
@@ -90,15 +95,46 @@ public class NewsLoaderTest {
 				fileNewsReader);
 
 		NewsLoader newsLoader = new NewsLoader();
-		
-		
-		//WHEN:
+
+		// WHEN:
 		PublishableNews publishableNews = newsLoader.loadNews();
 		Field field = getField(PublishableNews.class, "subscribentContent");
-		List<String> testSubscribentContent = (List<String>) field.get(publishableNews);
-		
-		
-		//THEN:
-		assertEquals(testSubscribentContent.size(),1);
+		List<String> testSubscribentContent = (List<String>) field
+				.get(publishableNews);
+
+		// THEN:
+		assertEquals(testSubscribentContent.size(), 1);
 	}
+
+	@Test
+	public void loadNews_shouldCallLoadConfigurationOnce() {
+		
+		//GIVEN:
+		IncomingNews incomingNews = new IncomingNews();
+
+		// mock Configuration
+		Configuration configuration = mock(Configuration.class);
+		when(configuration.getReaderType()).thenReturn("File");
+
+		// mock ConfigurationLoader
+		mockStatic(ConfigurationLoader.class);
+		ConfigurationLoader configurationLoader = mock(ConfigurationLoader.class);
+		when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+		when(configurationLoader.loadConfiguration()).thenReturn(configuration);
+
+		// mock FileNewsReader
+		FileNewsReader reader = mock(FileNewsReader.class);
+		when(reader.read()).thenReturn(incomingNews);
+
+		// mock NewsReaderFactory
+		mockStatic(NewsReaderFactory.class);
+		when(NewsReaderFactory.getReader((String) Mockito.any())).thenReturn(
+				reader);
+
+		NewsLoader loader = new NewsLoader();
+		loader.loadNews();
+
+		verify(configurationLoader, times(1)).loadConfiguration();
+	}
+
 }
