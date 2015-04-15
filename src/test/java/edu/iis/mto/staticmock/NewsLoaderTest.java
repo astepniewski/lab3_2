98c8eb1532cdafe445455e5caa134aa4ab756b01
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2015-04-15 11:18:48 +0200

    Dodanie testu dla metody loadNews klasy NewsLoader. Testuje czy czy news publiczny bez subskrybcji trafia do newsów nie subskrybowanych.

---
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -0,0 +1,63 @@
+package edu.iis.mto.staticmock;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import static org.junit.Assert.*;
+import edu.iis.mto.staticmock.reader.FileNewsReader;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.reflect.Whitebox.getField;
+
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ ConfigurationLoader.class, NewsReaderFactory.class })
+public class NewsLoaderTest {
+
+	@SuppressWarnings("unchecked" )
+	@Test
+	public void loadNews_expectedOnlyPublicContent() throws IllegalArgumentException, IllegalAccessException {
+
+		//GIVEN:
+		IncomingInfo incomingInfo = new IncomingInfo("content", SubsciptionType.NONE);
+		IncomingNews incomingNews = new IncomingNews();
+		incomingNews.add(incomingInfo);
+		
+		// mock Configuration
+		Configuration configuration = mock(Configuration.class);
+		when(configuration.getReaderType()).thenReturn("File");
+
+		// mock ConfigurationLoader
+		mockStatic(ConfigurationLoader.class);
+		ConfigurationLoader configurationLoader = mock(ConfigurationLoader.class);
+		when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+		when(configurationLoader.loadConfiguration()).thenReturn(configuration);
+
+		// mock FileNewsReader
+		FileNewsReader fileNewsReader = mock(FileNewsReader.class);
+		when(fileNewsReader.read()).thenReturn(incomingNews);
+
+		// mock NewsReaderFactory
+		mockStatic(NewsReaderFactory.class);
+		when(NewsReaderFactory.getReader((String) Mockito.any())).thenReturn(
+				fileNewsReader);
+
+		NewsLoader newsLoader = new NewsLoader();
+		
+		
+		//WHEN:
+		PublishableNews publishableNews = newsLoader.loadNews();
+		Field field = getField(PublishableNews.class, "publicContent");
+		List<String> testPublicContent = (List<String>) field.get(publishableNews);
+		
+		
+		//THEN:
+		assertEquals(testPublicContent.size(),1);
+	}
+}
